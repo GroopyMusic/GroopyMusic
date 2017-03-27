f5a3ff8156a4c40136eb41621888dba5f8d2235b
matthieudou <matthieu.doultremont@gmail.com>
2017-03-27 22:16:36 +0200

    création de la page suggestionBox + controleur dans public + entity + twig + ajout lien dans le menu + route

--- src/AppBundle/Controller/PublicController.php
+++ src/AppBundle/Controller/PublicController.php
@@ -1,9 +1,11 @@
 <?php
+// src/AppBundle/Controller/PublicController.php
 
 namespace AppBundle\Controller;
 
 use AppBundle\Entity\Step;
 use AppBundle\Entity\User;
+use AppBundle\Entity\SuggestionBox;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
@@ -42,4 +44,47 @@ class PublicController extends Controller
             'step' => $step,
         ));
     }
+
+    /**
+     * @Route("/", name="boite-a-idees")
+     */
+    public function boiteAction(){
+
+        $em = $this->getDoctrine()->getManager();
+
+        // Création de la suggestion box
+        $suggestionBox = new SuggestionBox();
+        //on ajoute toutes les données dans l'entité
+        $suggestionBox->setName("d'Oultremont");
+        $suggestionBox->setFirstname("Matthieu");
+        $suggestionBox->setEmail("matthieu.doultremont@gmail.com");
+        $suggestionBox->setObject("Optimisation bien cool !");
+        $suggestionBox->setMessage("Ajoute un HelloWorld ! C'est de la balle.");
+        $suggestionBox->setMailCopy(true);
+
+        //des tests doivent venir ici ? 
+
+        // fin des tests
+
+        //on persiste et on flush
+        $em->persist($suggestionBox);
+        $em->flush();
+
+        //petit message de bien envoyé
+        $this->addFlash('notice', 'Message bien envoyé ! <br>Merci !');
+
+        return $this->render('AppBundle:Public:boiteAIdees.html.twig');
+    }
 }
+
+
+
+
+
+
+
+
+
+
+
+
