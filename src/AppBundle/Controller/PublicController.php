c6a577323b38d5c32d3c2a30b8d5b2d090c7cbf1
matthieudou <matthieu.doultremont@gmail.com>
2017-03-28 23:32:38 +0200

    Création du formulaire de la suggestionBox

--- src/AppBundle/Controller/PublicController.php
+++ src/AppBundle/Controller/PublicController.php
@@ -11,6 +11,13 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use AppBundle\Services\MailTemplateProvider;
 use Symfony\Component\Security\Core\User\UserInterface;
+// uses du formulaire
+use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\FormType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextareaType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 
 class PublicController extends Controller
 {
@@ -46,7 +53,7 @@ class PublicController extends Controller
     }
 
     /**
-     * @Route("/", name="boite-a-idees")
+     * @Route("/", name="suggestionBox")
      */
     public function suggestionBoxAction(){
 
@@ -54,26 +61,41 @@ class PublicController extends Controller
 
         // Création de la suggestion box
         $suggestionBox = new SuggestionBox();
-        //on ajoute toutes les données dans l'entité
-        $suggestionBox->setName("d'Oultremont");
-        $suggestionBox->setFirstname("Matthieu");
-        $suggestionBox->setEmail("matthieu.doultremont@gmail.com");
-        $suggestionBox->setObject("Optimisation bien cool !");
-        $suggestionBox->setMessage("Ajoute un HelloWorld ! C'est de la balle.");
-        $suggestionBox->setMailCopy(true);
+        
+        // Création du formbuilder -> formFactory
+        $formbuilder = $this->get('form.factory')->createBuilder(FormType::class, $suggestionBox);
+
+        // ajout des champs du formulaire
+        $formbuilder
+            ->add('date',       DateType::class)
+            ->add('name',       TextType::class)
+            ->add('firstName',  TextType::class)
+            ->add('email',      TextType::class)
+            ->add('object',     TextAreaType::class)
+            ->add('message',    TextType::class)
+            ->add('mailCopy',   CheckboxType::class)
+            ->add('send',       SubmitType::class)
+        ;
 
         //des tests doivent venir ici ? 
-
         // fin des tests
 
+        // on génère le formulaire
+        $form = $formbuilder->getForm();
+
         //on persiste et on flush
-        $em->persist($suggestionBox);
-        $em->flush();
+        // $em->persist($suggestionBox);
+        // $em->flush();
+
+        // /!\ j'en suis ici !
+            // https://openclassrooms.com/courses/developpez-votre-site-web-avec-le-framework-symfony/creer-des-formulaires-avec-symfony#/id/r-3623367
+        // /!\ ici la suite
+
 
         //petit message de bien envoyé
-        $this->addFlash('notice', 'Message bien envoyé ! <br>Merci !');
+        $this->addFlash('notice', 'Message bien envoyé ! Merci !');
 
-        return $this->render('AppBundle:Public:suggestionBox.html.twig');
+        return $this->render('AppBundle:Public:suggestionBox.html.twig', array('form' => $form->createView(),));
     }
 }
 
