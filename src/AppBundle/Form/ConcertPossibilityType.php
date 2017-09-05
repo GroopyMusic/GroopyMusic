6a3cc5e0fcbe82a0f174f229b7a792d8a251ae44
Gounzy <gonzyer@gmail.com>
2017-09-05 17:44:00 +0200

    Added jQuery UI datePicker for crowdfunding creation, where only available dates are valid in the picker

--- src/AppBundle/Form/ConcertPossibilityType.php
+++ src/AppBundle/Form/ConcertPossibilityType.php
@@ -2,9 +2,12 @@
 
 namespace AppBundle\Form;
 
+use AppBundle\Entity\ContractArtistPossibility;
 use Doctrine\ORM\EntityRepository;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -15,22 +18,29 @@ class ConcertPossibilityType extends AbstractType
         $step = $options['step'];
 
         $builder
-            ->add('hall', EntityType::class, array(
+            /*->add('hall', EntityType::class, array(
                 'class' => 'AppBundle\Entity\Hall',
-                'choice_label' => 'Salle',
                 'query_builder' => function (EntityRepository $er) use ($step) {
                     return $er->createQueryBuilder('h')
                         ->where('h.step = :step')
                         ->setParameter('step', $step)
                         ->orderBy('h.name', 'ASC');
                 }
-            ));
+            ))*/
+            ->remove('date')
+            ->add('date', DateType::class, array(
+                'widget' => 'single_text',
+                'format' => 'dd/MM/yyyy',
+                'html5' => false,
+                'attr' => ['class' => 'datePicker', 'available-dates' => $step->getAvailableDatesFormatted()],
+            ))
+        ;
     }
 
     public function configureOptions(OptionsResolver $resolver) {
         $resolver->setDefaults(array(
             'step' => null,
-            'data_class' => 'AppBundle\Entity\ConcertPossibility'
+            'data_class' => \AppBundle\Entity\ConcertPossibility::class,
         ));
     }
 
