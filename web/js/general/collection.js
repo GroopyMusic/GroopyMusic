1de3c17de2f7940cb1665beeae0a762fc604a95d
Gounzy <gonzyer@gmail.com>
2017-10-19 17:45:02 +0200

    Fixed main workflow bugs - javascripts for collections (had to remove document.ready)

---
+++ web/js/general/collection.js
@@ -0,0 +1,8 @@
+function um_collection(options) {
+    $('.collection').collection($.extend({
+        up: '',
+        down: '',
+        add_at_the_end: true,
+        add: '<a href="#">Ajouter</a>'
+    }, options));
+}
