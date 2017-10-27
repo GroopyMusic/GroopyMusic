b0356b6f5515be73eef5e3d723bbbe3173284c47
Gounzy <gonzyer@gmail.com>
2017-10-27 16:44:01 +0200

    Frontend - menu & logos (xs & sm still to be worked on)

--- web/js/general/stickynav.js
+++ web/js/general/stickynav.js
@@ -7,38 +7,23 @@ $(window).scroll(function() {
         if(!$mainNav.hasClass('stickytop')) {
             $mainNav.addClass('stickytop');
 
-            $('#logo').addClass('stickytop');
-
-            $('.nav-item.left-nav-item').animate({
-                right: "+=100"
-            }, 2000, function() {
-
+            $('.nav-item').each(function() {
+                $(this).addClass('stickytop')
             });
 
-            $('.nav-item.right-nav-item').animate({
-                left: "+=100"
-            }, 2000, function() {
-
-            });
+            $('#menuLogo').fadeIn();
+            $('#logo').addClass('hiddenLogo');
         }
     }
     else if($mainNav.hasClass('stickytop')) {
         $mainNav.removeClass('stickytop');
-        $('#logo').animate({
-            top: "-=150"
-        }, 2000, function() {
+
+        $('#logo').removeClass('hiddenLogo');
+        $('#menuLogo').fadeOut();
+
+        $('.nav-item').each(function() {
+            $(this).removeClass('stickytop')
         });
 
-        $('.nav-item.left-nav-item').animate({
-            right: "-=100"
-        }, 2000, function() {
-
-        });
-
-        $('.nav-item.right-nav-item').animate({
-            left: "-=100"
-        }, 2000, function() {
-
-        });
     }
 });
