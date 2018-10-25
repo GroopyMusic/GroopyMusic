d9f057bb69de8154873282b618056cf6c3d78753
Gounzy <gonzyer@gmail.com>
2018-10-26 01:25:05 +0200

    Added YB google Maps embed on campaign page + Static Maps (for tickets) but static maps doesn't seem to work with file_get_contents() for now

--- src/AppBundle/Services/ImageHelper.php
+++ src/AppBundle/Services/ImageHelper.php
@@ -7,6 +7,8 @@ class ImageHelper
 {
     public static function base64($url)
     {
+        // TODO
+        return false;
         try {
             $data = file_get_contents($url);
             $base64 = 'data:image/png;base64,' . base64_encode($data);
